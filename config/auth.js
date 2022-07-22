38108a73c4b8e68995c825e98b0071fd8ec69832
M. Ali <ghazlani@hotmail.com>
2022-07-22 17:46:47 +0900

    fix bugs from the tutorials and prepare everthing for next step

--- config/auth.js
+++ config/auth.js
@@ -1,6 +1,8 @@
 module.exports = {
     ensureAuthenticated: function(req, res, next) {
       if (req.isAuthenticated()) {
+        res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0') // no caache so when logged out, cant browser back button to reload page, forces reload
+
         return next();
       }
       req.flash('error_msg', 'Please log in to view that resource');
